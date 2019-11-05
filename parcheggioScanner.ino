2dc9ff6341e1c944cd690e29409310f6a9288eef
Giovanni Burresi <giovanniburresi@gmail.com>
2019-11-05 16:25:49 +0100

    i2c e serial json enabled

--- parcheggioScanner.ino
+++ parcheggioScanner.ino
@@ -1,3 +1,33 @@
+// Included libraries
+#include <Wire.h>
+
+// #define SERIAL_OUTPUT 1
+
+#define MAX_DIST 300
+#define MAX_VALUE 13283
+#define MIN_DIST 10
+#define MIN_VALUE 16361
+
+
+// Device address
+#define I2C_ADDR             0x70
+
+// I2C registers descriptions
+#define EVENT_GET_RIGHT_EXTERNAL      0x41
+#define EVENT_GET_RIGHT_INTERNAL     0x42
+#define EVENT_GET_LEFT_EXTERNAL     0x40
+#define EVENT_GET_LEFT_INTERNAL     0x43
+
+#define OUT_TYPE uint8_t
+
+// Output variables
+OUT_TYPE VALUE_RIGHT_EXTERNAL = 0;
+OUT_TYPE VALUE_RIGHT_INTERNAL = 0;
+OUT_TYPE VALUE_LEFT_EXTERNAL  = 0;
+OUT_TYPE VALUE_LEFT_INTERNAL  = 0;
+
+uint8_t EVENT = 0;
+
 int pin = 7;
 int ledPin = 13;
 unsigned long pulse_length = 0;
@@ -17,10 +47,16 @@ int distance[4];
 int initialPulseDuration  = 900 ;// microseconds
 int initialPulseTolerance = 100 ;// microseconde
 
+
+#include <ArduinoJson.h>
+
 void setup() {
   pinMode(pin, INPUT);
   pinMode(ledPin, OUTPUT);
-  Serial.begin(115200);
+
+  #ifdef SERIAL_OUTPUT
+    Serial.begin(115200);
+  #endif
 }
 
 void loop() {
@@ -41,54 +77,94 @@ void loop() {
     }
     sensorValue[i] = pulse_value;
   }
-/////////////////////////////////////////////////////////////
 
-
-/* get sensor number fro bites 8 and 9
-9<<0
-8<<1
-0 = a
-1 = d
-2 = c
-3 = b
-
-*/
-
-short sensorId = 0;
-sensorId |= sensorValue[9] << 1;
-sensorId |= sensorValue[8] << 0;
-
-
-int count = 13;
-
-short data = 0;
-count = 0;
-
-for(i=10; i<16; i++){
-  data |= sensorValue[i] << count;
-  count++;
-  }  
-for(i=0; i<8; i++){
-  data |= sensorValue[i] << count;
-  count++;
+  /* get sensor number fro bites 8 and 9
+  9<<0
+  8<<1
+  0 = a
+  1 = d
+  2 = c
+  3 = b 
+  */
+  
+  short sensorId = 0;
+  sensorId |= sensorValue[9] << 1;
+  sensorId |= sensorValue[8] << 0;
+  
+  int count = 13;
+  
+  short data = 0;
+  count = 0;
+  
+  for(i=10; i<16; i++){
+    data |= sensorValue[i] << count;
+    count++;
+    }  
+  for(i=0; i<8; i++){
+    data |= sensorValue[i] << count;
+    count++;
+    }
+  
+  values[ sensorId ] = data;
+  
+  for(i = 0; i<4; i++){
+    distance[i] = map(values[ i ], MIN_VALUE, MAX_VALUE, MIN_DIST, MAX_DIST);
   }
+  // Output variables
+  VALUE_RIGHT_EXTERNAL = distance[RIGHT_EXTERNAL] > 255 ? (OUT_TYPE) 255 : (OUT_TYPE) distance[RIGHT_EXTERNAL];
+  VALUE_RIGHT_INTERNAL = distance[RIGHT_INTERNAL] > 255 ? (OUT_TYPE) 255 : (OUT_TYPE) distance[RIGHT_INTERNAL];
+  VALUE_LEFT_EXTERNAL  = distance[LEFT_EXTERNAL] > 255 ? (OUT_TYPE) 255 : (OUT_TYPE) distance[LEFT_EXTERNAL];
+  VALUE_LEFT_INTERNAL  = distance[LEFT_INTERNAL] > 255 ? (OUT_TYPE) 255 : (OUT_TYPE) distance[LEFT_INTERNAL];
 
-values[ sensorId ] = data;
+#ifdef SERIAL_OUTPUT
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+    
+  root["right-ext"] = VALUE_RIGHT_EXTERNAL;
+  root["right-int"] = VALUE_RIGHT_INTERNAL;
+  root["left-ext"] = VALUE_LEFT_EXTERNAL;
+  root["left-int"] = VALUE_LEFT_INTERNAL;
 
-for(i = 0; i<4; i++){
-  distance[i] = map(values[ i ], 16361, 13280, 0, 300);
+  root.prettyPrintTo(Serial);
+#endif
+
 }
 
 
-Serial.println("  ");
 
-for(i = 0; i<4; i++){
-  Serial.print(i);
-  Serial.print("  ");
-  Serial.print( values[ i ]);
-  Serial.print("  ");
-  Serial.println( distance[ i ]);
+// I2C management
+void receiveEvent(int countToRead) {
+  byte x;
+  while (0 < Wire.available()) {
+    x = Wire.read();
+    //Serial.println(x, HEX);
+  }
+  //String message = "Receive event: ";
+  //String out = message + x;
+  
+  EVENT = x;
 }
 
-
+void requestEvent() {
+  String event_s = "0xFF";
+  switch (EVENT) {
+    case EVENT_GET_RIGHT_EXTERNAL: 
+      Wire.write(VALUE_RIGHT_EXTERNAL);
+      break;
+    case EVENT_GET_RIGHT_INTERNAL: 
+      Wire.write(VALUE_RIGHT_INTERNAL);
+      break;
+    case EVENT_GET_LEFT_EXTERNAL: 
+      Wire.write(VALUE_LEFT_EXTERNAL);
+      break;
+    case EVENT_GET_LEFT_INTERNAL: 
+      Wire.write(VALUE_LEFT_INTERNAL);
+      break;
+    default:
+      Wire.write(0xFF);
+      //event_s = String(0xFF,HEX);
+      //Serial.println("Request event: " + event_s);
+      break;
+    }
+  //Serial.println("Request event OUT: " + event_s);
 }
